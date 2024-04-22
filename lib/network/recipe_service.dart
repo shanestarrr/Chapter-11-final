b135ddb293cc810d4518255a47ce117e99012556
shanestarrr <shanestara@gmail.com>
2024-04-22 11:32:31 +0800

    parger

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+import 'package:http/http.dart';
+
+const String apiKey = '3056bb1ad336125a892bb1b5480b6e89';
+const String apiId = '3efbf7d1';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  // 1
+  Future getData(String url) async {
+    // 2
+    final response = await get(Uri.parse(url));
+    // 3
+    if (response.statusCode == 200) {
+      // 4
+      return response.body;
+    } else {
+      // 5
+      log(response.body);
+    }
+  }
+
+  // 1
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    // 2
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    // 3
+    return recipeData;
+  }
+}
